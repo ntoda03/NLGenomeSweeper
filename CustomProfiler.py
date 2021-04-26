89bb5b0ef955abd6945c8b821eb5f8764489618c
Nicholas TODA <nicholas.toda@mnhn.fr>
2021-04-26 16:23:31 +0200

    profiler allowed to take reference domain seq

--- CustomProfiler.py
+++ CustomProfiler.py
@@ -36,53 +36,53 @@ profile_folder = '00_profile_creation'
 #
 ##########################################################################
 def main(argv):
-	
-	# Parse command line arguments
-	parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
-	
-	required_group = parser.add_argument_group('Required arguments')
-	required_group.add_argument("-verified", metavar="<fasta file>", required=True, type=str,
-		help="A fasta file of protein sequences of known NBS-LRR genes.")
-	required_group.add_argument("-prefix", metavar="<prefix>", required=True, type=str,
-		help="Prefix for output files.")
-		
-    consensus_group = parser.add_argument_group('Consensus arguments')
-    output_group.add_argument("-reference_nbarc", metavar="<fasta file>",  
+    program_dir = os.path.dirname(os.path.realpath(__file__)).replace(' ', '\ ')
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
+    
+    required_group = parser.add_argument_group('Required arguments')
+    required_group.add_argument("-verified", metavar="<fasta file>", required=True, type=str,
+        help="A fasta file of protein sequences of known NBS-LRR genes.")
+    required_group.add_argument("-prefix", metavar="<prefix>", required=True, type=str,
+        help="Prefix for output files.")
+        
+    consensus_group = parser.add_argument_group('Domain arguments')
+    consensus_group.add_argument("-reference_nbarc", metavar="<fasta file>",  
         default=program_dir + '/' + 'data/Vitis_vinifera_NB-ARC_consensus.fa', type=str,
         help="A reference NB-ARC domain protein sequence for comparison in fasta format. [Default uses Vitis vinifera domains]")
 
-	output_group = parser.add_argument_group('Output arguments')
-	output_group.add_argument("-overwrite", metavar="[T/F]",  default='F', type=str,
-		help="Whether to overwrite output files if they already exist. [Default F]")
-	output_group.add_argument("-outdir", metavar="<path>",  default='./NLGenomeSweeper', type=str,
-		help="Set an existing output directory for program output. [Default ./NLGenomeSweeper]")
-	
-	args = parser.parse_args()
-	program_dir = os.path.dirname(os.path.realpath(__file__)).replace(' ', '\ ')
-	
+    output_group = parser.add_argument_group('Output arguments')
+    output_group.add_argument("-overwrite", metavar="[T/F]",  default='F', type=str,
+        help="Whether to overwrite output files if they already exist. [Default F]")
+    output_group.add_argument("-outdir", metavar="<path>",  default='./NLGenomeSweeper', type=str,
+        help="Set an existing output directory for program output. [Default ./NLGenomeSweeper]")
+    
+    args = parser.parse_args()
+    
 
-	# Error handling. 
-	if '/' in args.prefix:
-		parser.print_help()
-		raise Exception( 'ERROR: prefix should not contain a path!')
-	if not os.path.isfile(args.verified):
-		parser.print_help()
-		raise Exception( 'ERROR: custom profile file ' + args.verified + ' does not exist!')
-	if not args.outdir.endswith('/'):
-		args.outdir += '/'
-	if os.path.exists(args.outdir + profile_folder) and (args.overwrite == "F"):
-		parser.print_help()
-		raise Exception( 'ERROR: program output already exists and program is not set to overwrite!')	
-	else:
-		if not os.path.exists(args.outdir):
-			os.mkdir(args.outdir)
-		if os.path.exists(args.outdir + profile_folder):
-			shutil.rmtree(args.outdir + profile_folder)
-		os.mkdir(args.outdir + profile_folder)
-	
-	# Run program 
-	subprocess.run('{}/scripts/createProfile.sh {} {} {} {} {} prot'.format( program_dir, args.verified,  
-		 args.outdir + profile_folder + '/', program_dir, args.prefix, args.reference_nbarc, "prot"), shell=True)
+    # Error handling. 
+    if '/' in args.prefix:
+        parser.print_help()
+        raise Exception( 'ERROR: prefix should not contain a path!')
+    if not os.path.isfile(args.verified):
+        parser.print_help()
+        raise Exception( 'ERROR: custom profile file ' + args.verified + ' does not exist!')
+    if not args.outdir.endswith('/'):
+        args.outdir += '/'
+    if os.path.exists(args.outdir + profile_folder) and (args.overwrite == "F"):
+        parser.print_help()
+        raise Exception( 'ERROR: program output already exists and program is not set to overwrite!')    
+    else:
+        if not os.path.exists(args.outdir):
+            os.mkdir(args.outdir)
+        if os.path.exists(args.outdir + profile_folder):
+            shutil.rmtree(args.outdir + profile_folder)
+        os.mkdir(args.outdir + profile_folder)
+    
+    # Run program 
+    subprocess.run('{}/scripts/createProfile.sh {} {} {} {} {} prot'.format( program_dir, args.verified,  
+         args.outdir + profile_folder + '/', program_dir, args.prefix, args.reference_nbarc, "prot"), shell=True)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
