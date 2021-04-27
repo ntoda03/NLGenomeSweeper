120efff8db4e1f35350548e67dca908102dbbf55
Nicholas TODA <nicholas.toda@mnhn.fr>
2021-04-27 10:19:52 +0200

    fixed profiler tests

--- tests/testing_harness.sh
+++ tests/testing_harness.sh
@@ -181,7 +181,7 @@ test_region_interproscan(){
 
 test_NLRGenomeSweeper(){
     outputdir_NLG=$outdir/testing_dir/NLG
-    genome=$programdir/data/TAIR10_chr1.sample4.fa
+    genome=$programdir/data/02_domain_identification/Candidate_sites.with_flanking_4.fa
     rm -R $outputdir_NLG 2> /dev/null
     mkdir -p $outputdir_NLG
     $programdir/../NLGenomeSweeper -genome $genome -outdir $outputdir_NLG -t 2 > /dev/null 2> /dev/null
@@ -194,10 +194,10 @@ test_NLRGenomeSweeper(){
 
 test_CustomProfiler(){
     outputdir_profiler=$outdir/testing_dir/profiler
-    genes=$programdir/data/TAIR10_TNLs.fa
+    genes=$programdir/data/TAIR_TNLs.fa
     rm -R $outputdir_profiler 2> /dev/null
     mkdir -p $outputdir_profiler
-    $programdir/../CustomProfiler -verified $genes -prefix tair_tnls -outdir $outputdir_profiler > /dev/null 2> /dev/null
+    $programdir/../CustomProfiler.py -verified $genes -prefix tair_tnls -outdir $outputdir_profiler > /dev/null 2> /dev/null
     assertTrue "Error: CustomProfiler failed" "[ -r '$outputdir_profiler/00_profile_creation/tair_tnls.fa' ]"
     test1=$(cat $outputdir_profiler/00_profile_creation/tair_tnls.fa)
     test2=$(cat $programdir/data/00_profile_creation/tair_tnls.fa)
